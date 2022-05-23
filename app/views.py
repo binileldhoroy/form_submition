d40592a00bc4e82e8b25952a1b268cc440dbd0e9
binileldhoroy <bi2277009@gmail.com>
2022-05-23 19:13:54 +0530

    final commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    context = {}
+    if request.method == 'POST':
+        master = request.POST.get('master')
+        one = request.POST.get('one')
+        two = request.POST.get('two')
+        three = request.POST.get('three')
+        four = request.POST.get('four')
+
+        res = []
+        
+        strings = [one, two, three, four]
+        for string in strings:
+            masterCopy = master
+            for alph in string:
+                flag = True
+                if alph in masterCopy:
+                    masterCopy = masterCopy.replace(alph,'',1)
+                else:
+                    flag = False
+                    break
+            if flag:
+                res.append({'name': string, 'value': 'yes'})
+                master = masterCopy
+            else:
+                res.append({'name': string, 'value': 'no'})
+        context = {'result': res}
+    return render(request, 'app/index.html',context)
